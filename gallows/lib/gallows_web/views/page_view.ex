7e303590ebdd7014dcbc291f00e8fed26e60644c
Michael Ries <michael@riesd.com>
2018-06-24 15:04:25 -0600

    customize layout

--- gallows/lib/gallows_web/views/page_view.ex
+++ gallows/lib/gallows_web/views/page_view.ex
@@ -1,3 +1,7 @@
 defmodule GallowsWeb.PageView do
   use GallowsWeb, :view
+
+ defp plural_of(1, noun), do: "1 #{noun}"
+ defp plural_of(n, noun) when n > 1, do: "#{n} #{noun}s"
+ defp plural_of(n, noun) when n < 1, do: {:safe, "<span style='color: red'>#{n} #{noun}s</span>"}
 end
