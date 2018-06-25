ca9beb48c44efb506d80db2691cf454550a59382
Michael Ries <michael@riesd.com>
2018-06-24 22:39:46 -0600

    use assigns

--- gallows/lib/gallows_web/controllers/page_controller.ex
+++ gallows/lib/gallows_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule GallowsWeb.PageController do
   use GallowsWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", product: "pear", quantity: 2
   end
 end
