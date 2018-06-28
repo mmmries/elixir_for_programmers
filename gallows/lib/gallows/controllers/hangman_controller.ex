2fc33508adc5a7b83c12902e7e6c74b61f600d39
Michael Ries <michael@riesd.com>
2018-06-28 13:50:08 -0600

    get the game playable over HTTP

--- gallows/lib/gallows/controllers/hangman_controller.ex
+++ gallows/lib/gallows/controllers/hangman_controller.ex
@@ -6,10 +6,27 @@ defmodule Gallows.HangmanController do
 
     conn
     |> put_session(:game, game)
-    |> render("game_field.html", tally: Hangman.tally(game))
+    |> render(:game, tally: Hangman.tally(game))
+  end
+
+  def make_move(conn, %{"make_move" => %{"guess" => letter}}) do
+    tally = attempt_move(conn, letter)
+
+    put_in(conn.params["make_move"]["guess"], "")
+    |> render(:game, tally: tally)
   end
 
   def new_game(conn, _params) do
     render conn, "new_game.html"
   end
+
+  defp attempt_move(conn, guess) do
+    game = get_session(conn, :game)
+    if valid_move?(guess) do
+      Hangman.make_move(game, guess)
+    else
+      Hangman.tally(game) |> Map.put(:game_state, :invalid_guess)
+    end
+  end
+  defp valid_move?(letter), do: letter =~ ~r/\A[a-z]\z/
 end
--- gallows/lib/gallows/views/hangman_view.ex
+++ gallows/lib/gallows/views/hangman_view.ex
@@ -1,7 +1,22 @@
 defmodule Gallows.HangmanView do
   use Gallows, :view
 
+  def render("game.html", %{tally: tally}=assigns) do
+    case tally.game_state do
+      :won -> render("game_won.html", assigns)
+      :lost -> render("game_lost.html", assigns)
+      _ -> render("game_field.html", assigns)
+    end
+  end
+  
+
   defp word_so_far(%{letters: letters}) do
     letters |> Enum.join(" ")
   end
+
+  defp prompt(%{game_state: :invalid_guess}), do: "Please guess a single lower-case letter"
+  defp prompt(%{game_state: :good_guess}), do: "Yup, you are getting closer"
+  defp prompt(%{game_state: :bad_guess}), do: "Nope, that letter's not in the word"
+  defp prompt(%{game_state: :initializing}), do: "Take a guess"
+  defp prompt(%{game_state: :already_used}), do: "You already guessed that letter"
 end
