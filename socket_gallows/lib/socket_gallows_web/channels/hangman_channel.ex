c79627f7a75e246001204764581800558e90f656
Michael Ries <michael@riesd.com>
2018-07-02 00:02:43 -0600

    hangman working via channel + Vue.js

--- socket_gallows/lib/socket_gallows_web/channels/hangman_channel.ex
+++ socket_gallows/lib/socket_gallows_web/channels/hangman_channel.ex
@@ -1,16 +1,31 @@
 defmodule SocketGallows.HangmanChannel do
+
   use Phoenix.Channel
 
-  def join("hangman:game", _args, socket) do
+  require Logger
+  
+  def join("hangman:game", _, socket) do
     {:ok, game} = Hangman.new_game()
-    socket = assign(socket, :game, game)
-    {:ok, socket}
+    socket = socket |> assign(:game, game)
+    {:ok, socket }
   end
 
-  def handle_in("tally", _args, socket) do
-    game = socket.assigns.game
-    tally = Hangman.tally(game)
-    push(socket, "tally", tally)    
-    {:noreply, socket}
+  def handle_in("tally", _, socket) do
+    tally = socket.assigns.game |> Hangman.tally()
+    push(socket, "tally", tally)
+    { :noreply, socket }
   end
+  
+  def handle_in("make_move", guess, socket) do
+    tally = socket.assigns.game |> Hangman.make_move(guess)
+    push(socket, "tally", tally)
+    { :noreply, socket }
+  end
+
+  def handle_in("new_game", _, socket) do
+    {:ok, game} = Hangman.new_game()
+    socket = socket |> assign(:game, game)
+    handle_in("tally", nil, socket)
+  end
+  
 end
